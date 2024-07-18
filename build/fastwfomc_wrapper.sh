131b6d0a85620971976011586072a55d50cfa70a
Paulius Dilkas <paulius.dilkas@gmail.com>
2024-07-18 09:41:36 -0400

    Preparing for a new round of experiments

--- build/fastwfomc_wrapper.sh
+++ build/fastwfomc_wrapper.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
-cd fastwfomc
-source virtualenvwrapper.sh
-workon fastwfomc
-for ((i=1; i<=$1; i++)); do
+for ((i=1; ; i++)); do
     echo "Domain size: $i"
-    ulimit -t $3 -c 0 && /usr/bin/time -f "Elapsed: %e" python wrapper.py $2 $i || exit 1
+    ulimit -t $2 -c 0 && /usr/bin/time -f "Elapsed: %e" julia --project=../deps/FastWFOMC.jl fastwfomc_wrapper.jl $1 $i || exit 1
 done
--- build/forclift_wrapper.sh
+++ build/forclift_wrapper.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-cd ../lib/Forclift
-for ((i=1; i<=$1; i++)); do
+cd ../deps/Forclift
+for ((i=1; ; i++)); do
     echo "Domain size: $i"
-    sed -i "s/\.\.\.,[[:digit:]]\+/\.\.\.,$i/g" $2
-    ulimit -t $3 -c 0 && /usr/bin/time -f "Elapsed: %e" java -jar target/scala-2.11/crane-assembly-1.0.jar -z --format-in mln $2 || exit 1
+    sed -i "s/\.\.\.,[[:digit:]]\+/\.\.\.,$i/g" $1
+    ulimit -t $2 -c 0 && /usr/bin/time -f "Elapsed: %e" java -jar target/scala-2.11/crane-assembly-1.0.jar -z --format-in mln $2 || exit 1
 done
