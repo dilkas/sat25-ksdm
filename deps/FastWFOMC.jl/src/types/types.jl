3433dfc8911a27b245d0a6c104f59c68f6b4a14c
Paulius Dilkas <paulius.dilkas@gmail.com>
2025-06-01 06:34:58 -0400

    Cleanup

--- deps/FastWFOMC.jl/src/types/types.jl
+++
@@ -1,3 +0,0 @@
-include("weights.jl")
-include("wfomc.jl")
-include("cardinality_constraint.jl")
--- deps/fastwfomc/python-flint-0.5.0/src/flint/utils/flint_exceptions.py
+++
@@ -1,8 +0,0 @@
-
-
-class DomainError(Exception):
-    """
-    Exception intended to be called when a method is called on a
-    ring or field for which the domain is invalid.
-    """
-    pass
